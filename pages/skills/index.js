1a27758a70eb6e65398c17aecd0138fe69105ac0
Ezedélio Garcia <96814597+Ezedcode@users.noreply.github.com>
2022-11-19 21:17:20 +0100

    update

--- pages/skills/index.js
+++ pages/skills/index.js
@@ -1,10 +1,25 @@
 import React from "react";
+
+import skills from "../../data/skills.json";
+
 import styles from "./styles.module.css";
 
 const Skills = () => {
   return (
     <div className={styles.skills}>
-      <div>Skills</div>
+      <div>
+        <h1>{skills.Myself}</h1>
+        <p>{skills.content.p1}</p>
+        <p>{skills.content.p2}</p>
+      </div>
+      <div>
+        <p>{skills.abilities.p3}</p>
+        <ul>
+          {skills.abilities.technologies.map((array, index) => (
+            // eslint-disable-next-line react/jsx-key
+            <li>{skills.abilities.technologies[index]}</li>))}
+        </ul>
+      </div>
     </div>
   );
 };
