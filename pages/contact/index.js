72bfeb198315a73ed19e2c38628d851e3f77489c
Ezedélio Garcia <96814597+Ezedcode@users.noreply.github.com>
2022-11-23 13:03:47 +0100

    update

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -8,6 +8,8 @@ import SendBtn from "../../components/button/SendBtn";
 
 const Contact = () => {
 
+  //const nodemailer = require("nodemailer");
+
   async function handleOnSubmit(e) {
     e.preventDefault();
     const formData = {};
@@ -22,10 +24,34 @@ const Contact = () => {
       method: 'post',
       body: JSON.stringify(formData)
     })
-
-    console.log(formData);
   }
 
+  /*async function main() {
+    console.log(formData);
+
+    let transporter = await nodemailer.createTestAccount({
+      host: "smtp.ethereal.email",
+      port: 587,
+      secure: false,
+      auth: {
+        user: testAccount.user,
+        pass: testAccount.pass,
+      },
+    });
+
+    let info = await transporter.sendMail({
+      from: '"Company" <dinog165@gmail.com>',
+      to: "ezedeliogarcia@gmail.com",
+      subject: "Hello",
+      text: "Hello world!",
+      html: "<b>Hello world?</b>",
+    })
+
+    console.log("Message sent: %s", info.massageId);
+
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  }
+*/
   return (
     <div className={`${styles.contact} ${page.pages}`}>
       <div>
